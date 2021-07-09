3c940acd2101ea2320334953d2caf90de5f28be5
Alex Renoki <rennokki@gmail.com>
2021-07-10 01:30:00 +0300

    Reverted jest to 15s timeout

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
         '^.+\\.tsx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testTimeout: 20 * 1000,
+    testTimeout: 15 * 1000,
     collectCoverage: true,
     maxWorkers: 1,
     testRunner: 'jest-circus/runner',
