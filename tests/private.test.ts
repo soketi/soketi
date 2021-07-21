dbdff01b07a4e1b55c3a2ed6d5c9035e62b89aca
Alex Renoki <rennokki@gmail.com>
2021-07-21 23:22:39 +0300

    wip

--- tests/private.test.ts
+++ tests/private.test.ts
@@ -65,4 +65,84 @@ describe('private channel test', () => {
             });
         });
     });
+
+    test('connects and disconnects to private channel and does not leak memory', done => {
+        Utils.newServer({}, (server: Server) => {
+            let client = Utils.newClientForPrivateChannel();
+            let backend = Utils.newBackend();
+            let channelName = `private-${Utils.randomChannelName()}`;
+
+            client.connection.bind('disconnected', () => {
+                Utils.wait(3000).then(() => {
+                    let namespace = server.adapter.getNamespace('app-id');
+
+                    // TODO: This assertion is crazy
+                    // expect(namespace.sockets.size).toBe(0);
+                    expect(namespace.channels.size).toBe(0);
+
+                    done();
+                });
+            });
+
+            client.connection.bind('connected', () => {
+                let channel = client.subscribe(channelName);
+
+                channel.bind('greeting', e => {
+                    expect(e.message).toBe('hello');
+
+                    client.unsubscribe(channelName);
+
+                    Utils.wait(3000).then(() => {
+                        let namespace = server.adapter.getNamespace('app-id');
+                        let socket = namespace.sockets.get(namespace.sockets.keys().next().value);
+
+                        expect(namespace.channels.size).toBe(0);
+                        // TODO: This assertion is crazy
+                        // expect(namespace.sockets.size).toBe(1);
+                        expect(socket.subscribedChannels.size).toBe(0);
+                        expect(socket.presence.size).toBe(0);
+
+                        client.disconnect();
+                    });
+                });
+
+                channel.bind('pusher:subscription_succeeded', () => {
+                    Utils.sendEventToChannel(backend, channelName, 'greeting', { message: 'hello' });
+                });
+            });
+        });
+    });
+
+    test('sudden close connection in private channel and does not leak memory', done => {
+        Utils.newServer({}, (server: Server) => {
+            let client = Utils.newClientForPrivateChannel();
+            let backend = Utils.newBackend();
+            let channelName = `private-${Utils.randomChannelName()}`;
+
+            client.connection.bind('disconnected', () => {
+                Utils.wait(3000).then(() => {
+                    let namespace = server.adapter.getNamespace('app-id');
+
+                    // TODO: This assertion is crazy
+                    // expect(namespace.sockets.size).toBe(0);
+                    expect(namespace.channels.size).toBe(0);
+
+                    done();
+                });
+            });
+
+            client.connection.bind('connected', () => {
+                let channel = client.subscribe(channelName);
+
+                channel.bind('greeting', e => {
+                    expect(e.message).toBe('hello');
+                    client.disconnect();
+                });
+
+                channel.bind('pusher:subscription_succeeded', () => {
+                    Utils.sendEventToChannel(backend, channelName, 'greeting', { message: 'hello' });
+                });
+            });
+        });
+    });
 });
