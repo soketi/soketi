2df475a7fb68ade37924f3769cc61fbe824ed2d0
Alex Renoki <rennokki@gmail.com>
2022-01-23 03:55:53 +0200

    fixed postgres schema

--- tests/fixtures/postgres_schema.sql
+++ tests/fixtures/postgres_schema.sql
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS apps (
     max_client_events_per_sec integer NOT NULL,
     max_read_req_per_sec integer NOT NULL,
     webhooks json,
-    max_presence_members_per_channel tinyint(1) NULL,
-    max_presence_member_size_in_kb tinyint(1) NULL,
-    max_channel_name_length tinyint(1) NULL,
-    max_event_channels_at_once tinyint(1) NULL,
-    max_event_name_length tinyint(1) NULL,
-    max_event_payload_in_kb tinyint(1) NULL,
-    max_event_batch_size tinyint(1) NULL
+    max_presence_members_per_channel integer DEFAULT NULL,
+    max_presence_member_size_in_kb integer DEFAULT NULL,
+    max_channel_name_length integer DEFAULT NULL,
+    max_event_channels_at_once integer DEFAULT NULL,
+    max_event_name_length integer DEFAULT NULL,
+    max_event_payload_in_kb integer DEFAULT NULL,
+    max_event_batch_size integer DEFAULT NULL
 );
 
 INSERT INTO apps (
