ed9b8b793eef0465dfcc1f09b4c1f727afb90c73
Alex Renoki <rennokki@gmail.com>
2022-01-30 23:53:44 +0200

    wip nats

--- src/adapters/nats-adapter.ts
+++ src/adapters/nats-adapter.ts
@@ -1,24 +1,19 @@
-import { connect, JSONCodec, NatsConnection } from 'nats';
+import { connect, JSONCodec, Msg, NatsConnection, StringCodec } from 'nats';
 import { LocalAdapter } from './local-adapter';
+import { Log } from '../log';
 import { PresenceMemberInfo } from '../channels/presence-channel-manager';
-import { PubsubBroadcastedMessage } from './horizontal-adapter';
+import { HorizontalAdapter, PubsubBroadcastedMessage } from './horizontal-adapter';
 import { Server } from '../server';
 import { WebSocket } from 'uWebSockets.js';
 
-export interface NatsRequestBody {
-    appId: string;
-}
+const Redis = require('ioredis');
 
-export interface NatsResponseBody {
-    requestId: string;
-    sockets?: Map<string, WebSocket>;
-    members?: [string, PresenceMemberInfo][];
-    channels?: [string, string[]][];
-    totalCount?: number;
-    exists?: boolean;
-}
+export class NatsAdapter extends HorizontalAdapter {
+    /**
+     * The channel to broadcast the information.
+     */
+    protected channel = 'nats-adapter';
 
-export class NatsAdapter extends LocalAdapter {
     /**
      * The NATS connection.
      */
@@ -30,109 +25,96 @@ export class NatsAdapter extends LocalAdapter {
     protected jc: any;
 
     /**
+     * The String codec.
+     */
+    protected sc: any;
+
+    /**
      * Initialize the adapter.
      */
     constructor(server: Server) {
         super(server);
 
-        this.jc = JSONCodec();
+        this.requestChannel = `${this.channel}#comms#req`;
+        this.responseChannel = `${this.channel}#comms#res`;
 
-        connect({ servers: ['demo.nats.io'] }).then((connection) => {
+        this.jc = JSONCodec();
+        this.sc = StringCodec();
+
+        connect({ servers: ['127.0.0.1'], port: 4222, user: 'test', pass: 'test' }).then((connection) => {
             this.connection = connection;
 
-            this.listenForMessagesToBroadcast();
-            this.listenForGetSocketsRequests();
-
-            // this.connection.jet
-        });
-
-        setInterval(() => {
-            this.getSockets('app-id').then(response => {
-                console.log(response.size);
-            });
-        }, 5_000);
-    }
-
-    async getSockets(appId: string, onlyLocal = false): Promise<Map<string, WebSocket>> {
-        return new Promise((resolve, reject) => {
-            super.getSockets(appId, true).then(localSockets => {
-                let sockets = localSockets;
-                let timeout = setTimeout(() => resolve(sockets), 100);
-
-                this.connection.request('getSockets', this.jc.encode({ appId }), { timeout: 1000, reply: appId, noMux: true }).then((r) => {
-                    let response: NatsResponseBody = this.jc.decode(r.data);
-
-                    if (response.sockets) {
-                        clearTimeout(timeout);
-                        timeout = setTimeout(() => resolve(sockets), 100);
-
-                        response.sockets.forEach(ws => sockets.set(ws.id, ws));
-                    }
-                });
-            });
-        });
-    }
-
-    listenForGetSocketsRequests(): void {
-        this.connection.subscribe('getSockets', {
-            callback: (_err, m) => {
-                let { appId }: NatsRequestBody = this.jc.decode(m.data);
-
-                super.getSockets(appId).then(sockets => {
-                    let localSockets: WebSocket[] = Array.from(sockets.values());
-
-                    m.respond(this.jc.encode({
-                        sockets: localSockets.map(ws => ({
-                            id: ws.id,
-                            subscribedChannels: ws.subscribedChannels,
-                            presence: ws.presence,
-                            ip: ws.ip,
-                            ip2: ws.ip2,
-                        })),
-                    }))
-                });
-            },
-        });
-    }
-
-    listenForMessagesToBroadcast(): void {
-        this.connection.subscribe('broadcastMessage.*.*', {
-            callback: (_err, m) => {
-                let decodedMessage: PubsubBroadcastedMessage = this.jc.decode(m.data);
-
-                if (typeof decodedMessage !== 'object') {
-                    return;
-                }
-
-                const { appId, channel, data, exceptingId } = decodedMessage;
-
-                if (!appId || !channel || !data) {
-                    return;
-                }
-
-                this.sendLocally(appId, channel, data, exceptingId);
-            },
+            this.connection.subscribe(this.requestChannel, { callback: (_err, msg) => this.onRequest(msg) });
+            this.connection.subscribe(this.responseChannel, { callback: (_err, msg) => this.onResponse(msg) });
+            this.connection.subscribe(this.channel, { callback: (_err, msg) => this.onMessage(msg) });
         });
     }
 
     /**
-     * Send a message to a namespace and channel.
+     * Listen for requests coming from other nodes.
      */
-    send(appId: string, channel: string, data: string, exceptingId: string|null = null): any {
-        this.connection.publish(`broadcastMessage.${appId}.${channel}`, this.jc.encode({
-            appId,
-            channel,
-            data,
-            exceptingId,
-        }));
+    protected onRequest(msg: any): void {
+        if (typeof msg === 'object') {
+            msg = JSON.stringify(this.jc.decode(msg.data));
+        }
 
-        this.sendLocally(appId, channel, data, exceptingId);
+        super.onRequest(this.requestChannel, msg);
     }
 
     /**
-     * Force local sending only for the Horizontal adapter.
+     * Handle a response from another node.
      */
-    sendLocally(appId: string, channel: string, data: string, exceptingId: string|null = null): any {
-        super.send(appId, channel, data, exceptingId);
+    protected onResponse(msg: any): void {
+        if (typeof msg === 'object') {
+            msg = JSON.stringify(this.jc.decode(msg.data));
+        }
+
+        super.onResponse(this.responseChannel, msg);
+    }
+
+    /**
+     * Listen for message coming from other nodes to broadcast
+     * a specific message to the local sockets.
+     */
+    protected onMessage(msg: any): void {
+        if ((msg && msg.data) || msg instanceof ArrayBuffer) {
+            msg = this.jc.decode(msg.data);
+        }
+
+        if (typeof msg === 'string') {
+            msg = JSON.parse(msg);
+        }
+
+        let message: PubsubBroadcastedMessage = msg;
+
+        const { uuid, appId, channel, data, exceptingId } = message;
+
+        if (uuid === this.uuid || !appId || !channel || !data) {
+            return;
+        }
+
+        super.sendLocally(appId, channel, data, exceptingId);
+    }
+
+    /**
+     * Broadcast data to a given channel.
+     */
+    protected broadcastToChannel(channel: string, data: any): void {
+        if (typeof data === 'string') {
+            data = JSON.parse(data);
+        }
+
+        this.connection.publish(channel, this.jc.encode(data))
+    }
+
+    /**
+     * Get the number of Discover nodes.
+     */
+    protected getNumSub(): Promise<number> {
+        return this.connection.request('$SYS.REQ.SERVER.PING.CONNZ').then(response => {
+            let { data } = JSON.parse(this.sc.decode(response.data)) as any;
+
+            return data.total;
+        });
     }
 }
