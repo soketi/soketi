69f90d4631991603d0040579a87d5423b8a2f554
Alex Renoki <rennokki@gmail.com>
2022-02-09 15:22:10 +0200

    Using subfunction

--- src/adapters/local-adapter.ts
+++ src/adapters/local-adapter.ts
@@ -141,13 +141,7 @@ export class LocalAdapter implements AdapterInterface {
      * Send a message to a namespace and channel.
      */
     send(appId: string, channel: string, data: string, exceptingId: string|null = null): any {
-        let nsp = this.namespaces.get(appId);
-
-        if (!nsp) {
-            return;
-        }
-
-        nsp.getChannelSockets(channel).then(sockets => {
+        this.getNamespace(appId).getChannelSockets(channel).then(sockets => {
             sockets.forEach((ws) => {
                 if (exceptingId && exceptingId === ws.id) {
                     return;
