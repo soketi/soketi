781ce0e2f1428729afdd22cc065174e3f2e8a6b7
Alex Renoki <rennokki@gmail.com>
2022-05-07 11:55:29 +0300

    Reverted unsubscribe to 60s

--- src/adapters/horizontal-adapter.ts
+++ src/adapters/horizontal-adapter.ts
@@ -230,7 +230,7 @@ export abstract class HorizontalAdapter extends LocalAdapter {
                         this.unsubscribeFromApp(appId);
                     }
                 });
-            }, 3_000); // TODO: Customizable unsubscriptions interval
+            }, 60_000); // TODO: Customizable unsubscriptions interval
         }
 
         return Promise.resolve();
