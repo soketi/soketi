6abc0d312a21bd798e5bf355f75d29f91d19ca0e
Alex Renoki <alex@renoki.org>
2022-08-14 19:30:21 +0300

    Updated grace period to 1s

--- tests/utils.ts
+++ tests/utils.ts
@@ -66,6 +66,8 @@ export class Utils {
             'database.postgres.password': process.env.TEST_POSTGRES_PASSWORD || 'testing',
             'database.postgres.database': process.env.TEST_POSTGRES_DATABASE || 'testing',
             'queue.sqs.queueUrl': process.env.TEST_SQS_URL || 'http://localhost:4566/000000000000/test.fifo',
+            'debug': process.env.TEST_DEBUG || false,
+            'shutdownGracePeriod': 1_000,
         };
 
         return (new Server(options)).start((server: Server) => {
